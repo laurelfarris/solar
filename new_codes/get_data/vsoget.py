918f2538a790a0fd2c8a0df8f035ab8feaed0b49
laurelfarris <laurel07@nmsu.edu>
2016-07-21 16:38:07 -0600

    power outage may have hurt things

--- new_codes/get_data/vsoget.py
+++ new_codes/get_data/vsoget.py
@@ -1,6 +1,6 @@
 '''
 Programmer:     Laurel Farris
-Last modified:  27 June 2016
+Last modified:  20 July 2012
 
 '''
 from sunpy.net import vso
@@ -9,34 +9,52 @@ import astropy.units as u
 import pdb
 
 '''
-This code was constructed using the syntax provided by
-SunPy.org, modelled after vsoget.pro from Sam.
+This code was constructed using the syntax provided by SunPy.org,
+modelled after vsoget.pro provided by Sam (author?)
 Download data. If you have set your default download directory in your
 sunpyrc configuration file then you do not need to identify a path at all.
 Add '.wait()' at the end to keep the script from continuing until the
 data has been downloaded (if needed).
 '''
 
-# def VSOsearch():
-''' Create a new VSOClient instance and specify attributes '''
-client = vso.VSOClient()
+def VSOsearch(tstart, tend, inst, wave=None):
 
-tstart = parse_time('2012/06/01 01:00:00')
-tend = parse_time('2012/06/01 01:59:59')
-inst = 'aia'
-wave_min = 193
-wave_max = 193
-# sample = '12'  # Cadence desired (in seconds).
+    ''' Create a new VSOClient instance. This handles the particulars of how the data
+    from the data provider is downloaded to your computer. '''
+    client = vso.VSOClient()
 
-my_query = client.query(
-  vso.attrs.Time(tstart, tend),
-  vso.attrs.Instrument(inst),
-  vso.attrs.Wave(wave_min*u.AA, wave_max*u.AA)
-)
+    ''' Take string arg and return a datetime object '''
+    tstart = parse_time('2012/06/01 01:00:00')
+    tend = parse_time('2012/06/01 01:59:59')
 
-''' Print the number of matches '''
-print ("Number of records found: {}".format(len(my_query)))
-pdb.set_trace()
+    ''' Specify instrument, wavelength(s), and cadence (seconds) '''
+    inst = inst
+    if not wave:
+        
+    
+    wave_min = wave
+    wave_max = wave
+    # sample = '12'
 
-# def VSOget():
-res = client.get(my_query, path="/Users/laurel/sunpy/data/{instrument}/{file}.fits")
+    ''' Print the number of matches '''
+    print ("Number of records found: {}".format(len(my_query)))
+    #pdb.set_trace()
+
+    ''' Return a LIST of response objects, each of which is a record found by the VSO '''
+    return client.query(
+      vso.attrs.Time(tstart, tend),
+      vso.attrs.Instrument(inst),
+      vso.attrs.Wave(wave_min*u.AA, wave_max*u.AA)
+      )
+
+def VSOget():
+    #res = client.get(my_query, path="/Users/laurel/sunpy/data/{instrument}/{file}.fits")
+    return 0
+
+
+
+my_query = VSOsearch('2012/06/01 01:00:00','2012/06/01 01:00:59', 'aia')
+
+
+
+
