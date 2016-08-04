758818356107e57c122e0e9572a7cda62640d506
laurelfarris <laurel07@nmsu.edu>
2016-08-04 17:13:04 -0600

    new codes

--- new_codes/align/align_modules.py
+++ new_codes/align/align_modules.py
@@ -6,26 +6,23 @@ def shift_sub(image, x0, y0):
     '''
     Purpose:            Shift a (2D) image with subpixel accuracies
     Calling sequence:   Result = shift_sub(image, x0, y0)
-    Notes:              Thoroughly converted from the IDL version
-                        (whose syntax is in docstrings right above the python bits),
-                        but not very efficiently.
-    Last updated:       07 July 2016
+    Notes:              Thoroughly converted from the IDL version,
+                        although dimensions are questionable
+    Last updated:       04 August 2016
     '''
 
-    image = np.array(image)
     if int(x0)-x0 == 0. & int(y0)-y0 == 0.:
         image = np.roll(image, x0, axis=1)
         image = np.roll(image, y0, axis=0)
         return image
 
-    ''' Get dimensions of image '''
     s = image.shape
-    x = np.broadcast_to( np.arange(s[1]), (s[0], s[1]) )
-    y = (np.broadcast_to( np.arange(s[0]), (s[1], s[0]) )).transpose()
+    x = np.matrix(np.ones(s[0])).reshape(s[0], 1) * np.matrix(np.arange(s[1]))
+    y = np.matrix(np.arange(s[0])).reshape(s[0], 1) * np.matrix(np.ones(s[1]))
 
     ''' Set interpolation values '''
-    x = np.max([0, (x-x0), (s[1]-1.)])
-    y = np.max([0, (y-y0), (s[0]-1.)])
+    x1 = np.max([0, (x-x0), (s[1]-1.)])
+    y1 = np.max([0, (y-y0), (s[0]-1.)])
 
     ''' return interpolated image '''
     return interp2d(x1, y1, image, kind='cubic')
@@ -36,21 +33,33 @@ def alignoffset(image, reference):
     si = image.shape
     sr = reference.shape
 
-    if si[1] != sr[1] & si[0] != sr[0]:
+    ''' Check that images have the same dimensions '''
+    if not (si[1] == sr[1] and si[0] == sr[0]):
         print 'Incompatible Images : alignoffset'
         return [0, 0.]
 
-    nx = 2.**int(np.log(si[1])/np.log(2.))
-    nx = nx*(1+(si[1] > nx))
-    ny = 2**int(np.log(si[2])/np.log(2.))
-    ny = ny*(1+(si[2] > ny))
-
+    nx = 2**int(np.log(si[1])/np.log(2.))
+    nx = nx * (1 + (si[1] > nx))
+    ny = 2**int(np.log(si[0])/np.log(2.))
+    ny = ny * (1 + (si[0] > ny))
 
     image1 = image - np.mean(image)
     reference1 = reference - np.mean(image)
-    if nx != si[1] | ny != si[2]:
+    if nx != si[1] or ny != si[2]:
         image1 = congrid(image1, nx, ny, cubic=-0.5)
         reference1 = congrid(reference1, nx, ny, cubic=-0.5)
+    
+    sigma_x = nx/6.
+    sigma_y = ny/6.
+    xx = (np.matrix(np.arange(nx))).reshape(nx.shape[0],1) * np.matrix(np.ones(ny))
+    xx = xx - nx * (xx > nx/2)
+    yy = (np.matrix(np.ones(nx))).reshape(nx.shape[0],1) * np.matrix(np.arange(ny))
+    yy = yy - ny * (yy > ny/2)
+    ### window = np.roll()
+    window = np.sqrt(window)
+
+    
+
     return cor
 
 
---
+++ new_codes/align/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+
+y = 9
+x = 9
+
+if x != 4 or y != 5:
+    print 'blah'
