fc63a527e45d66ab702a26fb8084ab387d377c49
Laurel Farris <laurel@laurelfarris.com>
2016-08-31 09:45:32 -0600

    stuff

--- new_codes/bp_size/plots.py
+++ new_codes/bp_size/plots.py
@@ -9,8 +9,8 @@ import matplotlib.ticker as mtick
 #matplotlib.rc('font',**{'family':'serif','serif':['Times']})
 #matplotlib.rc('text', usetex=True)
 
-fig, ax = plt.subplots(figsize=(8,12), nrows=3, ncols=2, sharex=True, sharey=True)
-
+#fig, ax = plt.subplots(figsize=(8,12), nrows=3, ncols=2, sharex=True, sharey=True)
+fig = plt.figure(figsize=(8,10))
 waves = ['94', '131', '171', '193', '211', '304'] #, '335']
 #axes = [ax[0][0], ax[0][1], ax[0][2], ax[1][0], ax[1][1], ax[1][2]]
 
@@ -22,8 +22,6 @@ for i in range(0, len(waves)):
     ax = fig.add_subplot(3,2,i+1)
     m = ax.scatter(r, cor, c=t, vmin=min(t), vmax=max(t),
                     cmap='viridis', s=3, lw=0)
-    ax.axis('tight')  # no extra space between data points and axes
-
     '''
     majorLocator = mtick.LinearLocator(numticks=10)
     axes[i].xaxis.set_major_locator(majorLocator)
@@ -31,17 +29,17 @@ for i in range(0, len(waves)):
     axes[i].yaxis.set_major_locator(majorLocator)
     axes[i].yaxis.set_minor_locator(minorLocator)
     '''
-
     ax.minorticks_on()
     ax.tick_params(axis='both', which='minor', direction='in', length=4)
     ax.tick_params(axis='both', which='major', direction='in', length=6)
     #ax.set_xticks(np.arange(0,80,10), minor=True)
     #ax.set_xticklabels(fontsize=tfont)
     #ax.set_yticklabels(np.arange(0,1,0.1),fontsize=tfont)
-    ax.text(68, 0.93, waves[i] + ' \AA{} ', ha='right')
+    ax.axis('tight')  # no extra space between data points and axes
+    ax.text(68, 0.93, waves[i] + ' $\mathrm{\AA{}}$', ha='right')
 
-fig.text(0.5, 0.03, 'radius [pixels]', ha='center', fontsize=fontsize)
-fig.text(0.1, 0.5, 'maximum cross-correlation', va='center', rotation='vertical', fontsize=fontsize)
+#fig.text(0.5, 0.03, 'radius [pixels]', ha='center', fontsize=fontsize)
+#fig.text(0.1, 0.5, 'maximum cross-correlation', va='center', rotation='vertical', fontsize=fontsize)
 
 cax = fig.add_axes([0.93, 0.10, 0.02, 0.80])
 #step=30
@@ -51,6 +49,6 @@ cbar = fig.colorbar(m, cax=cax)
 #cbar.ax.set_xticklabels(['Low', 'Medium', 'High'])  # horizontal colorbar
 
 
-plt.subplots_adjust(wspace=0.1, hspace=0.1)
-#plt.show(block=False)
-plt.savefig('bp_size.png', bbox_inches='tight', dpi=300)
+plt.subplots_adjust(wspace=0.2, hspace=0.2)
+plt.show(block=False)
+#plt.savefig('bp_size.png', bbox_inches='tight', dpi=300)
