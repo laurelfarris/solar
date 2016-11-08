8750ace5329126ee8213223ccdf9bcbe255c47e2
Laurel Farris <laurel@laurelfarris.com>
2016-11-08 16:51:44 -0700

    research

--- new_python_codes/bp_size/plots.py
+++ new_python_codes/bp_size/plots.py
@@ -12,7 +12,7 @@ from matplotlib.ticker import FormatStrFormatter
 #fig, ax = plt.subplots(figsize=(8,12), nrows=3, ncols=2, sharex=True, sharey=True)
 
 fig = plt.figure(figsize=(10,8))
-fontsize=14
+fontsize=12
 waves = ['94', '131', '171', '193', '211', '304'] #, '335']
 x = [0.10, 0.35, 0.65, 0.10, 0.35, 0.65]
 y = [0.10, 0.10, 0.10, 0.45, 0.45, 0.45]
@@ -20,8 +20,10 @@ y = [0.10, 0.10, 0.10, 0.45, 0.45, 0.45]
 for i in range(0, len(waves)):
     f = open(waves[i] + '_bp_sizes.dat')
     x, y, r, cor, t = np.loadtxt(f, unpack=True)
+    '''
     r = r*0.6
     r = r.astype(float)
+    '''
 
     ax = fig.add_subplot(2,3,i+1)
     m = ax.scatter(r, cor, c=t, vmin=min(t), vmax=max(t),
@@ -29,7 +31,7 @@ for i in range(0, len(waves)):
     #ax.axis('tight')
     ax.set_xlim(left=0, right=0.5*max(r))
     ax.set_ylim(bottom=0.5, top=1)
-    ax.set_aspect(40./1.)
+    ax.set_aspect(70./1.)
     '''
     '''
     ax.text(0.9, 0.9, waves[i] + ' $\mathrm{\AA{}}$', ha='center', transform=ax.transAxes )
@@ -50,7 +52,7 @@ for i in range(0, len(waves)):
     ax.set_yticklabels(np.arange(0,1,0.1),fontsize=12)
     '''
 
-fig.text(0.5, 0.024, 'radius [arcseconds] (1" = ~435 km)', ha='center', fontsize=fontsize)
+fig.text(0.5, 0.024, 'radius [pixels] (1 pixel = 0.6")', ha='center', fontsize=fontsize)
 fig.text(0.03, 0.5, 'maximum cross-correlation', va='center', rotation='vertical', fontsize=fontsize)
 
 #fig.subplots_adjust(left=0.1, right=0.95, bottom=0.08, top=0.75, wspace=0.1, hspace=0.1)
